897488bd0f0bb4832e1bc9fa8dd0de320641d064
itxsuhail <32263639+itxsuhail@users.noreply.github.com>
2026-04-14 18:26:37 +0530

    Add data object to store form input values

--- AlphaHub.co.in/frontend/script.js
+++ AlphaHub.co.in/frontend/script.js
@@ -1,7 +1,10 @@
 function scrollToForm() {
     document.getElementById("form-section").scrollIntoView();
   }
-  
+  const data = {
+  name: document.getElementById("name").value,
+  phone: document.getElementById("phone").value
+};
   document.getElementById("leadForm").addEventListener("submit", function(e) {
     e.preventDefault();
     alert("Request submitted!");
